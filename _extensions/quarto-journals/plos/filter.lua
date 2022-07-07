02c93790c821b6bcd0a8783e59c3c410c751eb34
Christophe Dervieux <christophe.dervieux@gmail.com>
2022-07-07 23:46:30 +0200

    Add supplementary information support using custom blocks

--- _extensions/quarto-journals/plos/filter.lua
+++ _extensions/quarto-journals/plos/filter.lua
@@ -2,45 +2,100 @@
 
 -- these classes, when placed on a span will be replaced
  -- with an identical LaTeX command for PDF output
-local texMappings = {
-  "proglang",
-  "pkg",
-  "fct",
-  "class"
+ local texMappings = {
+  "nameref"
 }
+
+function print_debug(label,obj,iter)
+  local debug_mode = true
+  obj = obj or nil
+  iter = iter or pairs
+  label = label or ""
+  label = "DEBUG (from custom-environment.lua): "..label
+  if (debug_mode) then
+      if not obj then
+          print(label.." nil")
+      elseif (type(obj) == "string") then
+          print(label.." "..obj)
+      elseif type(obj) == "table" then
+          for k,v in iter(obj) do
+              print(label.."id:"..k.. " val:"..v)
+          end
+      end
+  end
+  return nil
+end
 
 return {
   {
-    Span = function(el) 
-      -- read the span contents and emit correct output
+    Span = function(el)
       local contentStr = pandoc.utils.stringify(el.content)
-
-      for i, mapping in ipairs(texMappings) do
-        if #el.attr.classes == 1 and el.attr.classes:includes(mapping) then
-          if quarto.doc.isFormat("pdf") then
-            return pandoc.RawInline("tex", "\\" .. mapping .. "{" .. contentStr .. "}" )
-          else 
-            return pandoc.Code(contentStr);
+        for i, mapping in ipairs(texMappings) do
+          if #el.attr.classes == 1 and el.attr.classes:includes(mapping) then
+            if quarto.doc.isFormat("pdf") then
+              return pandoc.RawInline("tex", "\\" .. mapping .. "{" .. contentStr .. "}" )
+            else 
+              el.content = pandoc.Str( conentStr)
+              return el
           end
         end
       end
     end,
-    Meta = function(meta) 
-      -- Authors output in the template uses a special separator
-      -- to join authors (including wrapping to a new line)
-      -- this computes the proper prefix and places it in the author metadata
-      -- for use by the template
-      local byAuthor = meta['by-author']
-      for i, author in ipairs(byAuthor) do
-        local prefix = {pandoc.RawInline("tex ","")};
-        if i > 1 and i % 2 == 1 then
-          prefix = {pandoc.RawInline("tex", "\\AND")}
-        elseif i > 1 then
-          prefix = {pandoc.RawInline("tex", "\\And")}
+    Div = function(div)
+      -- Special treatment for supplementary material
+      if (div.classes:includes("supp")) then
+        if quarto.doc.isFormat("pdf") then
+          local headerNum = 0
+          local header = pandoc.List()
+          local labelId
+          local paraNum = 0
+          local para = pandoc.List()
+          -- First Take element
+          div.content:walk {
+            Header = function(el)
+              if (headerNum > 0) then
+                error('Only one header can be set in supplementary section divs')
+              end
+              --[[
+              el.level = 4
+              headerNum = headerNum + 1
+              header:insert(el)
+              ]]
+              el.content:insert(1, pandoc.RawInline('tex', '\\paragraph*{'))
+              el.content:insert(pandoc.RawInline('tex', '}'))
+              header:extend(el.content)
+              labelId = el.identifier
+              -- Remove header
+              return {}
+            end,
+            Para = function(el)
+              if (paraNum == 0) then
+                -- first paragraph is title sentence
+                if (el.content and el.content[1].t ~= "Strong") then
+                  el.content = pandoc.Inlines(pandoc.Strong(el.content))
+                end
+                el.content:insert(1, pandoc.RawInline('tex', '{'))
+                el.content:insert(pandoc.RawInline('tex', '}'))
+              elseif (paraNum == 1) then
+                -- ok
+              else
+                error('Only two paragraph are allowed in supplementary section div')
+              end
+              
+              para:insert(el)
+              paraNum = paraNum + 1
+              -- remove Para
+              return {}
+            end
+          }
+          -- Build the new paragraph content
+          header:extend({pandoc.Str("\n"), pandoc.RawInline("tex", "\\label{"..labelId.."}"), pandoc.Str("\n")})
+          para = pandoc.utils.blocks_to_inlines(para, {pandoc.Space()})
+          header:extend(para)
+          -- Return the new para in place of the Div
+          return pandoc.Para(header)
         end
-        author['metadata']['latex-prefix'] = prefix
       end
-      return meta
-    end
+    end,
   }
 }
